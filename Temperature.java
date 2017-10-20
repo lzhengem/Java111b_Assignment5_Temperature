b5fec5d69f62c002577ca9f1f0d62b311db8647e
kryptonianS <32245925+kryptonianS@users.noreply.github.com>
2017-10-20 00:25:14 -0700

    Add files via upload
    okay so this is an update version of the file Temperature.
    Let me know if its correct or not. and do provide feed back.
    thank you

--- Temperature.java
+++ Temperature.java
@@ -1,41 +1,67 @@
 
 import java.util.Scanner;
 
+
+
+
+
+
 public class Temperature {
-double degree;
-char type;
+    double degree;
+        char type;
 
-// to specify both parameters (degrees, type)
-public Temperature(double degree, char type){
-    this.degree = degree;
-    this.type = type;
+           
+
+
+    // to specify both parameters (degrees, type)
+    public Temperature(double degree, char type){
+        degree = degree;
+            
+        type = type;
     
-}
+            }
 
-//to specify just the degrees (defaults to type Celsius)
-public Temperature(double degree){
-    this.degree= degree;
-    this.type ='C';
+    //to specify just the degrees (defaults to type Celsius)
+        public Temperature(double degree){
+            degree = degree;
+            
+            type = 'C';
    
-}
+                }
 
-//to specify just the type (defaults to 0.0 for the degrees value)
-public Temperature(char type){
-    this.type = type;
-    this.degree = 0.0;
-}
+    //to specify just the type (defaults to 0.0 for the degrees value)
+        public Temperature(char type){
+            type = type;
+            
+                degree = 0.0;
+                    }
 
-// the default constructor that specifies neither (defaults to 0 degrees Celsius)
-public Temperature(){
-    this.type ='C';
-    this.degree= 0;
-}  
+    // the default constructor that specifies neither (defaults to 0 degrees Celsius)
+        public Temperature(){
+            type ='C';
+              
+                 degree= 0;
+                        }  
 
-//prompt user for degrees and type and then reads the values
-public void ReadInPut(){
-    Scanner input = new Scanner(System.in);
-    this.degree = input.nextDouble();
-    this.type = (char) input.nextShort();
-}
+    //prompt user for degrees and type and then reads the values
+        public void readInput(){
+           
+            
+           Scanner input = new Scanner(System.in);
+                
+           
+            System.out.println("Enter a temperature in degress ");
+                
+               degree = input.nextDouble();
+    
+                                  
+                System.out.println("Enter 'F' (or 'f') for Fahrenheit or 'C' (or 'c') for Celsius:");
+                
+                //convert user input to a char
+                char type[]=input.next().toCharArray();
+             
+                  System.out.print("Temperature =" + " " + degree + String.valueOf(type));
+                }   
 
+        
 }
