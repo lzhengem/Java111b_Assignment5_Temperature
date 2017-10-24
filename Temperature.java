bab2be593ebac00cd3daef97e57aa185b74f5a9f
kryptonianS <32245925+kryptonianS@users.noreply.github.com>
2017-10-23 17:13:28 -0700

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -19,35 +19,35 @@ public class Temperature {
     // to specify both parameters (degrees, type)
     public Temperature(double degree, char type){
        degree = degree;
-        type = type;
+       type = type;
     }
 
     //to specify just the degrees (defaults to type Celsius)
     public Temperature(double degree){
        degree = degree;
-        type = 'C';
+       type = 'C';
     }
 
     //to specify just the type (defaults to 0.0 for the degrees value)
     public Temperature(char type){
        type = type;
-        degree = 0.0;
+       degree = 0.0;
     }
 
     // the default constructor that specifies neither (defaults to 0 degrees Celsius)
     public Temperature(){
        type ='C';
-        degree= 0;
+       degree= 0;
     }  
 
     //prompt user for degrees and type and then reads the values
     public void readInput(){
-    //prompts user to for tempature in degrees
-     System.out.println("Enter a temperature in degress ");
-     degree = input.nextDouble();
-    //prompts user for degrees in fahrentheit (f) or Celsius (c)
+     //prompts user to for tempature in degrees
+      System.out.println("Enter a temperature in degress ");
+      degree = input.nextDouble();
+     //prompts user for degrees in fahrentheit (f) or Celsius (c)
       System.out.println("Enter 'F' (or 'f') for Fahrenheit or 'C' (or 'c') for Celsius:");
-    //convert user input to a char
+     //convert user input to a char
       type = input.next().toUpperCase().charAt(0);
     }   
         
